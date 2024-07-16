641c758f04024d8c135e93df49c7b5a5739820f6
Peterson Kirill <145568807+PetersonKirill@users.noreply.github.com>
2024-07-16 20:08:41 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,7 +1,8 @@
 my_string = input()
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(" ", ""))
-print(my_string[0])
-print(my_string[-1])
+
+print(len(my_string)) #количество символов в строке
+print(my_string.upper()) #все символы заглавные в строке
+print(my_string.lower()) #все символы строчные в строке
+print(my_string.replace(" ", "")) #удалены все пробелы
+print(my_string[0]) #отображает первый символ в строке
+print(my_string[-1]) #отображает последний символ в строке
